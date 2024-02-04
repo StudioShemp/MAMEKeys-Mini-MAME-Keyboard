9736ea9b81b08ee0a920ab698abf9f616e4a5846
Alan <44254504+StudioShemp@users.noreply.github.com>
2024-02-04 16:48:38 +0800

    Update MAMEKeys.ino
    Improved keypress and debounce under MAME

--- src/MAMEKeys.ino
+++ src/MAMEKeys.ino
@@ -1,7 +1,5 @@
 // A mini keyboard HID device to provide the standard MAME (non-player control) keys to a cabinet. Ideal for cabinets with Zero Delay Controllers to maintain default key mappings within MAME. Also includes Consumer keys for control of the system volume.  
 
-
-
 #include "HID-Project.h"  //https://github.com/NicoHood/HID
 
 // MAME Key assignment for the pins (// and default keyboard key in comments)
@@ -22,17 +20,39 @@ const int VolUp = A0;    // HID System Master Volume Control Up
 const int VolDown = A1;  // HID System Master Volume Control Down
 const int VolMute = A2;  // HID System Master Volume Mute Toggle
 
+
+//Visual Pinball keys
+const int LeftFlipper = A4;
+const int RightFlipper = A5;
+
+
 // for later expansion - eg BigBox exit (Key X), mouse keys, movement
 // const int Unused0 = 0;
 // const int Unused1 = 1;
 // const int Unused13 = 13; (optionally used - see untested comments below)
 // const int UnusedA3 = A3;
-// const int UnusedA4 = A4;
-// const int UnusedA5 = A5;
+
 
 // delay between button repeats if held down
 const int ButtonRepeatDelay = 300;
-const int PauseRepeatDelay = 1000;
+const int PauseRepeatDelay = 300;
+
+int Coin1Press = 0;
+int Coin2Press = 0;
+int Player1Press = 0;
+int Player2Press = 0;
+int ExitPress = 0;
+int PausePress = 0;
+int SelectPress = 0;
+int MenuPress = 0;
+int MameVolPress = 0;
+int ServicePress = 0;
+int ServiceCoinPress = 0;
+int VolUpPress = 0;
+int VolDownPress = 0;
+int VolMutePress = 0;
+int LeftFlipperPress = 0;
+int RightFlipperPress = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -52,6 +72,8 @@ void setup() {
   pinMode(VolUp, INPUT_PULLUP);
   pinMode(VolDown, INPUT_PULLUP);
   pinMode(VolMute, INPUT_PULLUP);
+  pinMode(LeftFlipper, INPUT_PULLUP);
+  pinMode(RightFlipper, INPUT_PULLUP);
 
   Consumer.begin();  // Initiate consumer HID device on host
 
@@ -61,117 +83,203 @@ void setup() {
   // pinMode(Unused13, INPUT_PULLUP); //Optionally used to delay start of a
   // second Zero Delay Joystick Encoder pinMode(UnusedA2, INPUT_PULLUP);
   // pinMode(UnusedA3, INPUT_PULLUP);
-  // pinMode(UnusedA4, INPUT_PULLUP);
-  // pinMode(UnusedA5, INPUT_PULLUP);
+
 
   Keyboard.begin();
 
-  /*
-  //This section Yet to be tested - just a thought bubble, not tried/implemented
+/*---------------------This section Yet to be tested - just a thought bubble, not tried/implemented-----------------------------------
+  
+  Optionally set Pin 13 as Output and High (5v on the ATMega32U4 boards) and  use the output to enable power a second Zero Delay Joystick Encoder (ie cut the USB +5V line going to the encoder and connect the encoder board to this pin.) Useful if the order your zero delay encoders are detected by the OS is random, and P1 / P2 controls sometimes swap.
 
-  //Optionally set Pin 13 as Output and High (5v on the ATMega32U4 boards) and
-  use the output to enable power a second Zero Delay Joystick Encoder (ie cut
-  the USB +5V line going to the encoder and connect the encoder board to this
-  pin.) Useful if the order your zero delay encoders are detected by the OS is
-  random, and P1 / P2 controls sometimes swap.
-
-  //Pin D13 (ref C7) appears to remain low at  so until set high, power
-  won't flow to the second controller. Setting the "delay(60000);" line to an
-  appropriate value allows the pin to go high after the OS has ed and has
+  Pin D13 (ref C7) appears to remain low at  so until set high, power won't flow to the second controller. Setting the "delay(60000);" line to an appropriate value allows the pin to go high after the OS has ed and has
   detected the first controller.
 
-  //DANGER Will Robinson!!! - the output will probably not have the current to
-  drive the board directly - doing so would probably smoke the Leonardo board
-  (or maybe just the pin). I haven't measured the current drawn by a ZD encoder
-  under load, but the pin can only supply 40mA... and only 20mA for continuos
-  current draw. If attempting this, use this output to drive a transistor/FET to
-  supply power - and at your own risk!
+  DANGER Will Robinson!!! - the output will probably not have the current to drive the board directly - doing so would probably smoke the Leonardo board (or maybe just the pin). I haven't measured the current drawn by a ZD encoder under load, but the pin can only supply 40mA... and only 20mA for continuos current draw. If attempting this, use this output to drive a transistor/FET to supply power - and at your own risk!
 
-  delay(60000); // millisecons - Set to at least the time it takes to  the
-  OS and detect the first USB Controller digitalWrite(13, HIGH); //power pin 13
-  and whatever is connected to it
   */
 
+ //delay(60000); // millisecons - Set to at least the time it takes to  theOS and detect the first USB Controller 
+ //digitalWrite(13, HIGH); //power pin 13 and whatever is connected to it
+
+/*---------------------------------------------------------------------------------------------------------------*/
+  
   Serial.println("Starting Keyboard...");
 }
 
 void loop() {
-  if (digitalRead(Coin1) == LOW) {
+  if ((digitalRead(Coin1) == LOW) && (Coin1Press == 0)) {
     Keyboard.press('5');
+	delay(ButtonRepeatDelay);
     Serial.println("Coin1");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	Coin1Press = 1;
+	}
+	
+  if ((digitalRead(Coin1) == HIGH) && (Coin1Press == 1)) {	
+    Keyboard.release('5');
+	Coin1Press = 0;
+
   }
 
-  if (digitalRead(Coin2) == LOW) {
+  if ((digitalRead(Coin2) == LOW) && (Coin2Press == 0)) {
     Keyboard.press('6');
+	delay(ButtonRepeatDelay);
     Serial.println("Coin2");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	Coin2Press = 1;
+	}
+	
+  if ((digitalRead(Coin2) == HIGH) && (Coin2Press == 1)) {	
+    Keyboard.release('6');
+	Coin2Press = 0;
   }
 
-  if (digitalRead(Player1) == LOW) {
+  if ((digitalRead(Player1) == LOW) && (Player1Press == 0)) {
     Keyboard.press('1');
+	delay(ButtonRepeatDelay);
     Serial.println("Player1");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	Player1Press = 1;
+	}
+	
+  if ((digitalRead(Player1) == HIGH) && (Player1Press == 1)) {
+    Keyboard.release('1');
+	Player1Press = 0;
   }
 
-  if (digitalRead(Player2) == LOW) {
+  if ((digitalRead(Player2) == LOW) && (Player2Press == 0)) {
     Keyboard.press('2');
+	delay(ButtonRepeatDelay);
     Serial.println("Player2");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	Player2Press = 1;
+	}
+	
+  if ((digitalRead(Player2) == HIGH) && (Player2Press == 1)) {
+    Keyboard.release('2');
+	Player2Press = 0;
   }
 
-  if (digitalRead(Exit) == LOW) {
+  if ((digitalRead(Exit) == LOW) && (ExitPress == 0)) {
     Keyboard.press(KEY_ESC);
+	delay(ButtonRepeatDelay);
     Serial.println("Exit");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	ExitPress = 1;
+	}
+	
+  if ((digitalRead(Exit) == HIGH) && (ExitPress == 1)) {
+    Keyboard.release(KEY_ESC);
+	ExitPress = 0;
   }
 
-  if (digitalRead(Pause) == LOW) {
+  if ((digitalRead(Pause) == LOW) && (PausePress == 0)) {
     Keyboard.press('P');
+	delay(PauseRepeatDelay);
     Serial.println("Pause");
-    Keyboard.releaseAll();
-    delay(PauseRepeatDelay);
+	PausePress = 1;
+	}
+	
+  if ((digitalRead(Pause) == HIGH) && (PausePress == 1)) {
+    Keyboard.release('P');
+	PausePress = 0;
   }
 
-  if (digitalRead(Select) == LOW) {
+  if ((digitalRead(Select) == LOW) && (SelectPress == 0)) {
     Keyboard.press(KEY_RETURN);
+	delay(ButtonRepeatDelay);
+    Serial.println("Pause");
+	SelectPress = 1;
+	}
+	
+  if ((digitalRead(Select) == HIGH) && (SelectPress == 1)) {
+    Keyboard.release(KEY_RETURN);
+	SelectPress = 0;
+  }
+
+
+/* Alternative Select for Visual Pinball and digital shoother plunger (e.g. my design at www.thingiverse.com/thing:6419048)
+  if ((digitalRead(Select) == LOW) && (SelectPress == 0)) {  // this key altered for digital pinball plunger. when pin goes low, plunger is drawn. Wait for plunger to go high again to launch. Use N/O microswitch contacts
+	Keyboard.press(KEY_RETURN);
+	SelectPress=1;
+  }
+  
+  if ((SelectPress == 1) && (digitalRead(Select)) == HIGH) {
     Serial.println("Select");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+    Keyboard.release(KEY_RETURN);
+	SelectPress = 0;
   }
+*/
 
-  if (digitalRead(Menu) == LOW) {
+  if ((digitalRead(Menu) == LOW) && (MenuPress == 0)) {
     Keyboard.press(KEY_TAB);
+	delay(ButtonRepeatDelay);
     Serial.println("Menu");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	MenuPress = 1;
+	}
+	
+   if ((digitalRead(Menu) == HIGH) && (MenuPress == 1)) {
+    Keyboard.release(KEY_TAB);
+ 	MenuPress = 0;
   }
 
-  if (digitalRead(Service) == LOW) {
+  if ((digitalRead(Service) == LOW) && (ServicePress == 0)) {
     Keyboard.press(KEY_F2);
+	delay(ButtonRepeatDelay);
     Serial.println("Service");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	ServicePress = 1;
+	}
+	
+  if ((digitalRead(Service) == HIGH) && (ServicePress == 1)) {
+    Keyboard.release(KEY_F2);
+	ServicePress = 0;	
   }
 
-  if (digitalRead(ServiceCoin) == LOW) {
+  if ((digitalRead(ServiceCoin) == LOW) && (ServiceCoinPress == 0)) {
     Keyboard.press('9');
+	delay(ButtonRepeatDelay);
     Serial.println("ServiceCoin");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	ServiceCoinPress = 1;
+	}
+	
+  if ((digitalRead(ServiceCoin) == HIGH) && (ServiceCoinPress == 1)) {
+    Keyboard.release('9');
+	ServiceCoinPress = 0;
   }
 
-  if (digitalRead(MameVol) == LOW) {
+  if ((digitalRead(MameVol) == LOW) && (MameVolPress == 0)) {
     Keyboard.press('~');
+	delay(ButtonRepeatDelay);
     Serial.println("MameVol");
-    Keyboard.releaseAll();
-    delay(ButtonRepeatDelay);
+	MameVolPress = 1;
+	}
+	
+  if ((digitalRead(MameVol) == HIGH) && (MameVolPress == 1)) {
+    Keyboard.release('~');
+	MameVolPress = 0;	
   }
+  
+  
+
+  if ((digitalRead(LeftFlipper) == LOW) && (LeftFlipperPress == 0)) {
+    Keyboard.press(KEY_LEFT_SHIFT);
+	delay(ButtonRepeatDelay);
+    Serial.println("LeftFlipper");
+	LeftFlipperPress = 1;
+	}
+	
+  if ((digitalRead(LeftFlipper) == HIGH) && (LeftFlipperPress == 1)) {
+    Keyboard.release(KEY_LEFT_SHIFT);
+	LeftFlipperPress = 0;
+  }
+
+  if ((digitalRead(RightFlipper) == LOW) && (RightFlipperPress == 0)) {
+    Keyboard.press(KEY_RIGHT_SHIFT);
+	delay(ButtonRepeatDelay);
+    Serial.println("RightFlipper");
+	RightFlipperPress = 1;
+	}
+	
+  if ((digitalRead(RightFlipper) == HIGH) && (RightFlipperPress == 1)) {
+    Keyboard.release(KEY_RIGHT_SHIFT);
+	RightFlipperPress = 0;	
+  }
+  
 
   // this section sets the system volume for the default sound device - ie the
   // OS master volume control.  Tested in Win 10 and 11.
